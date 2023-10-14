a51ee9c7e453c9314cdf30d667292bbf4837f8de
Diego-notebook <Diego@DESKTOP-RN26VKI>
2023-10-14 16:50:01 -0300

    inclusão das funções de login e gerar holerite

--- functions/loginRequest.js
+++ functions/loginRequest.js
@@ -1,37 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btnLogin = document.getElementById('btn-login');
+    const btnLogin = document.getElementById('btn-login');
+    const loginInput = document.getElementById('login');
+    const senhaInput = document.getElementById('senha');
+    const credentialAlert = document.getElementById('alerta');
+    
+    btnLogin.addEventListener('click', async function () {
+      const login = loginInput.value;
+      const senha = senhaInput.value;
   
-  btnLogin.addEventListener('click', function () {
-      const login = document.getElementById('login').value;
-      const senha = document.getElementById('senha').value;
-      
-      const data = {
-          login,
-          senha
-      };
-      
-      fetch('http://localhost:8080/login', {
+      credentialAlert.style.display = 'none';
+  
+      try {
+        const data = { login, senha };
+        
+        const response = await fetch('http://localhost:8080/login', {
           method: 'POST',
           headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
           },
           body: JSON.stringify(data)
-      })
-      .then(res => res.json())
-      .then(data => {
-        //console.log(data);
-        if (data.statusCode == 200) {
-            localStorage.setItem('funcionario_id', data.funcionario_id);
-            // ação de login;
+        });
+
+        const responseData = await response.json();
+
+        if (responseData.statusCode === 200) {
+            localStorage.setItem('funcionario_id', responseData.funcionario_id);
+            window.location.href = './principal.html';
         } else {
-            console.log('login ou senha invalidos');
-            // açao de não autorizado
+            credentialAlert.style.display = 'block';
         }
-        //const teste = localStorage.getItem('funcionario_id');
-        //console.log(teste);
-      })
-      .catch(error => {
-          console.error('Erro na requisição:', error);
-      });
-  });
-});
+      } catch (error) {
+            console.error('Erro na requisição:', error);
+      }
+    });
+  });  
